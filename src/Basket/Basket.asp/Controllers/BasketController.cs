4c787b0217ce86ee441f95f1945368c16cf66f15
shalini <writetoshalini@gmail.com>
2021-09-27 15:52:31 -0500

    Completed till basket api

---
+++ src/Basket/Basket.asp/Controllers/BasketController.cs
@@ -0,0 +1,55 @@
+﻿using Basket.api.Entities;
+using Basket.api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository repository;
+
+        public BasketController(IBasketRepository basket)
+        {
+            this.repository = basket ?? throw new ArgumentNullException(nameof(basket));
+        }
+
+        [HttpGet("{username}", Name ="GetBasket")]
+        [ProducesResponseType (typeof(ShoppingCart),(int)HttpStatusCode.OK )]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var basket = await repository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            // TODO : Communicate with Discount.Grpc
+            // and Calculate latest prices of product into shopping cart
+            // consume Discount Grpc
+            //foreach (var item in basket.Items)
+            //{
+            //    var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            //    item.Price -= coupon.Amount;
+            //}
+
+            return Ok(await repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
---
+++ src/Basket/Basket.asp/Repositories/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
